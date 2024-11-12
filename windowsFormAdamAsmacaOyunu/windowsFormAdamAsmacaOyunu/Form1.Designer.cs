23d404541a23db65419d3eaf2ce9e6e819941e4f
ardaddemiray <ardademiray62@gmail.com>
2024-11-12 11:58:07 +0300

    CSharpWindowsForms_Hangman_Game
    C# ve Windows Forms kullanılarak geliştirilmiş bir adam asmaca oyunudur.

---
+++ windowsFormAdamAsmacaOyunu/windowsFormAdamAsmacaOyunu/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace windowsFormAdamAsmacaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.işlemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adamAsmacaOynaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.canlbl = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.işlemToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(751, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(56, 21);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // işlemToolStripMenuItem
+            // 
+            this.işlemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adamAsmacaOynaToolStripMenuItem});
+            this.işlemToolStripMenuItem.Name = "işlemToolStripMenuItem";
+            this.işlemToolStripMenuItem.Size = new System.Drawing.Size(50, 21);
+            this.işlemToolStripMenuItem.Text = "İşlem";
+            // 
+            // adamAsmacaOynaToolStripMenuItem
+            // 
+            this.adamAsmacaOynaToolStripMenuItem.Name = "adamAsmacaOynaToolStripMenuItem";
+            this.adamAsmacaOynaToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.adamAsmacaOynaToolStripMenuItem.Text = "Adam asmaca oyna";
+            this.adamAsmacaOynaToolStripMenuItem.Click += new System.EventHandler(this.adamAsmacaOynaToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Eşleşen Harfler : ";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(248, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 30);
+            this.label4.TabIndex = 6;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(176, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 30);
+            this.label2.TabIndex = 7;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(212, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 30);
+            this.label3.TabIndex = 8;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(284, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 30);
+            this.label5.TabIndex = 9;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(76, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Girilen Harf :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(179, 140);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 29);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(285, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 27);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(76, 243);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 20);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Süre :";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(133, 243);
+            this.progressBar1.Maximum = 120;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(237, 23);
+            this.progressBar1.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(76, 307);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(69, 20);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Tahmin :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 304);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 29);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(462, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 276);
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // canlbl
+            // 
+            this.canlbl.AutoSize = true;
+            this.canlbl.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.canlbl.Location = new System.Drawing.Point(458, 4);
+            this.canlbl.Name = "canlbl";
+            this.canlbl.Size = new System.Drawing.Size(88, 21);
+            this.canlbl.TabIndex = 24;
+            this.canlbl.Text = "Can Sayısı :";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(546, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 21);
+            this.label7.TabIndex = 25;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(319, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 30);
+            this.label6.TabIndex = 26;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "hayvan1.JPG");
+            this.ımageList1.Images.SetKeyName(1, "hayvan2.jpg");
+            this.ımageList1.Images.SetKeyName(2, "hayvan3.jpg");
+            this.ımageList1.Images.SetKeyName(3, "hayvan4.jpg");
+            this.ımageList1.Images.SetKeyName(4, "hayvan5.jpg");
+            this.ımageList1.Images.SetKeyName(5, "hayvan6.jpg");
+            this.ımageList1.Images.SetKeyName(6, "hayvan7.jpg");
+            this.ımageList1.Images.SetKeyName(7, "hayvan8.jpg");
+            this.ımageList1.Images.SetKeyName(8, "hayvan9.jpg");
+            this.ımageList1.Images.SetKeyName(9, "hayvan10.jpg");
+            this.ımageList1.Images.SetKeyName(10, "hayvan11.jpg");
+            this.ımageList1.Images.SetKeyName(11, "hayvan12.jpg");
+            this.ımageList1.Images.SetKeyName(12, "hayvan13.jpg");
+            this.ımageList1.Images.SetKeyName(13, "hayvan14.jpg");
+            this.ımageList1.Images.SetKeyName(14, "hayvan15.jpg");
+            this.ımageList1.Images.SetKeyName(15, "hayvan16.jpg");
+            this.ımageList1.Images.SetKeyName(16, "hayvan17.jpg");
+            this.ımageList1.Images.SetKeyName(17, "hayvan18.jpg");
+            this.ımageList1.Images.SetKeyName(18, "hayvan19.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(751, 419);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.canlbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem işlemToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label canlbl;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adamAsmacaOynaToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ windowsFormAdamAsmacaOyunu/windowsFormAdamAsmacaOyunu/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsFormAdamAsmacaOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] hayvanlar = { "akrep", "aslan", "balık", "böcek", "domuz", "geyik", "goril", "horoz", "karga", "koyun", "köpek", "kumru", "sığır", "sinek", "tavuk", "tilki", "yılan", "yunus", "zebra" };
+        Random hayvan = new Random();
+        string secilenHayvan;
+        int index;
+        int kalanCan = 5;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            index = hayvan.Next(0,hayvanlar.Length);
+            secilenHayvan = hayvanlar[index];
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void adamAsmacaOynaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+            label7.Text = kalanCan.ToString();
+            progressBar1.Value = 0;
+            timer1.Stop();
+            pictureBox1.Image = null;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            timer1.Start();
+
+            Label[] labelList = new Label[] { label2, label3, label4, label5, label6 };
+
+            char girilenHarf = textBox2.Text.ToCharArray()[0];
+
+            bool harfEslendiMi = false;
+
+            for (int i = 0; i < secilenHayvan.Length; i++)
+            {
+                if (secilenHayvan[i] == girilenHarf)
+                {
+                    harfEslendiMi= true;
+                    labelList[i].Text = girilenHarf.ToString();
+                }
+            }
+
+            if (!harfEslendiMi) {
+                kalanCan--;
+                label7.Text=kalanCan.ToString();
+
+                if (kalanCan == 0)
+                {
+                    MessageBox.Show("Can hakkınız bitti, oyunu kaybettiniz!");
+                    pictureBox1.Image = ımageList1.Images[index];
+                }
+
+                textBox2.Clear();
+                
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value >= progressBar1.Maximum)
+            {
+               timer1.Stop();
+                textBox2.Enabled = false;
+                textBox1.Visible = true;
+                MessageBox.Show("Süreniz doldu. Lütfen tahmin ediniz");
+            }
+            else
+            {
+                progressBar1.Value++;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode== Keys.Enter)
+            {
+                string tahmin = textBox1.Text.ToLower();
+
+                if(tahmin == secilenHayvan.ToLower())
+                {
+                    MessageBox.Show("Tebrikler, doğru tahmin ettiniz! Kelime : " + secilenHayvan.ToUpper());
+                    pictureBox1.Image = ımageList1.Images[index];
+
+                }
+                else
+                {
+                    MessageBox.Show("Tahmininiz yanlış! Doğru kelime : " + secilenHayvan.ToUpper());
+                    pictureBox1.Image = ımageList1.Images[index];
+
+                }
+            }
+        }
+    }
+}
