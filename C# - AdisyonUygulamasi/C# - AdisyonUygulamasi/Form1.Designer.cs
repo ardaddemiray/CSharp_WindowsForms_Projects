03d2966d3ba1a0b696e1f367487c68ae9eb5de59
ardaddemiray <ardademiray62@gmail.com>
2024-11-13 11:20:55 +0300

    CSharpWindowsForms_Billing_App
    C# ve Windows Forms kullanılarak geliştirilmiş bir yiyecek-içecek adisyon uygulamasıdır.

---
+++ C# - AdisyonUygulamasi/C# - AdisyonUygulamasi/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace C____AdisyonUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Toplam :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(330, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 160);
+            this.listBox1.TabIndex = 8;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Tost-Kaşarlı - 50 TL",
+            "Tost-Sucuklu - 60 TL",
+            "Tost-Sucuklu-Kaşarlı - 75 TL",
+            "Gözleme-Kaşarlı - 85 TL",
+            "Gözleme-Peynirli - 90 TL",
+            "Gözleme-Patatesli - 80 TL",
+            "Gözleme-Ispanaklı - 80 TL",
+            "Soğuk Sandviç - 100 TL"});
+            this.checkedListBox2.Location = new System.Drawing.Point(158, 17);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(166, 169);
+            this.checkedListBox2.TabIndex = 7;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Çay - 20 TL",
+            "Türk Kahvesi - 50 TL",
+            "Limonata - 40 TL",
+            "Cappucino - 80 TL",
+            "Filtre Kahve - 50 TL",
+            "Soda - 25 TL",
+            "Meyveli Soda - 50 TL",
+            "Su - 20 TL",
+            "Kola - 70 TL",
+            "Fanta - 70TL",
+            "Sade gazoz - 70 TL"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 17);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(140, 169);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ C# - AdisyonUygulamasi/C# - AdisyonUygulamasi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C____AdisyonUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] yemekFiyatlari = { 50, 60, 75, 85, 90, 80, 80, 100 };
+            double[] icecekFiyatlari = { 20, 50, 40, 80, 50, 25, 50, 20, 70, 70, 70 };
+
+            double toplamYemek = 0;
+            double toplamIcecek = 0;
+
+            foreach (int index in checkedListBox1.CheckedIndices)
+            {
+                int icecekIndeks = index;
+                toplamIcecek += icecekFiyatlari[icecekIndeks];
+            }
+
+            foreach (int index in checkedListBox2.CheckedIndices)
+            {
+                int yiyecekIndeks = index;
+                toplamYemek += yemekFiyatlari[yiyecekIndeks];
+            }
+
+            double toplamUcret = toplamYemek + toplamIcecek;
+            textBox1.Text = toplamUcret.ToString() + " TL";
+            listBox1.Items.Clear();
+
+            foreach (var seciliIcecek in checkedListBox1.CheckedItems)
+            {
+                listBox1.Items.Add(seciliIcecek.ToString());
+            }
+
+            foreach (var seciliYemek in checkedListBox2.CheckedItems)
+            {
+                listBox1.Items.Add(seciliYemek.ToString());
+            }
+        }
+    }
+}
